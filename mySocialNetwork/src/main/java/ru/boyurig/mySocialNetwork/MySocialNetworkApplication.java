a0964a6f188f1dc0985a5f67e4d3a77d1f981031
Yurii <iyureg@gmail.com>
2024-05-02 17:02:46 +0300

    initial commit & add part1 simple rest controller

---
+++ mySocialNetwork/src/main/java/ru/boyurig/mySocialNetwork/MySocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package ru.boyurig.mySocialNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySocialNetworkApplication.class, args);
+	}
+
+}
