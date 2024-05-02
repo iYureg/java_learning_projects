77e64061f990a33877be53436783ea3303297d8c
Yurii <iyureg@gmail.com>
2024-05-02 11:10:39 +0300

    add: roles management part 7

--- buysell/src/main/java/ru/boyurig/buysell/configurations/SecurityConfig.java
+++ buysell/src/main/java/ru/boyurig/buysell/configurations/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
@@ -17,6 +18,7 @@ import ru.boyurig.buysell.services.CustomUserDetailsService;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
@@ -30,6 +32,7 @@ public class SecurityConfig {
                                "/registration",
                                "/products",
                                "/product/{id}",
+                               "/user/**",
                                "/css/**",
                                "/js/**",
                                "/images/**").permitAll()
--- buysell/src/main/java/ru/boyurig/buysell/services/UserService.java
+++ buysell/src/main/java/ru/boyurig/buysell/services/UserService.java
@@ -8,6 +8,12 @@ import ru.boyurig.buysell.models.User;
 import ru.boyurig.buysell.models.enums.Role;
 import ru.boyurig.buysell.repositories.UserRepository;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -28,4 +34,35 @@ public class UserService {
         return true;
     }
 
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public void banUser(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+
+        if(user != null) {
+            if(user.isActive()) {
+                user.setActive(false);
+                log.info("Ban user with id: {}; email: {}", user.getId(), user.getEmail());
+            } else {
+                user.setActive(true);
+                log.info("Unban user with id: {}; email: {}", user.getId(), user.getEmail());
+            }
+            userRepository.save(user);
+        }
+    }
+
+    public void changeUserRole(User user, Map<String, String> form) {
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for(String key : form.keySet()) {
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+    }
 }
